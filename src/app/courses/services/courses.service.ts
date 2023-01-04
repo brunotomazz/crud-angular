614a6c30df871bd7d285792913592e18a74f5034
bruno.tomaz <stomaz.bruno@gmail.com>
2023-01-04 11:36:37 -0300

    included spinner on list loading

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { first, take, tap } from 'rxjs';
+import { delay, first, take, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +15,7 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
+      delay(200),
       tap(courses => console.log(courses)
       )
     )
